fb25ed7167121388193e138cda21c4b6c2989865
Jacob Angel <jacobryanangel@gmail.com>
2022-05-01 01:35:55 -0500

    tyson has been added

--- bot.py
+++ bot.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
+client = discord.Client()
+
 
 bot = commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'),
                    help_command=None)
@@ -14,6 +16,10 @@ bot = commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'),
 async def price(ctx):
     await ctx.send(f"Joe Biden")
 
-
-
+@client.event
+async def on_voice_state_update(member, before, after):
+  if not before.channel and after.channel and member.id == 951690798175506472:
+    channel = client.get_channel(709505921092550658)
+    await channel.send('Tyson has small balls.')
+    
 bot.run(TOKEN)
