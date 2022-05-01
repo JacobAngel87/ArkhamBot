f7f261d8ea75ec95ead9076c921f34cf69dc179c
Jacob Angel <jacobryanangel@gmail.com>
2022-05-01 02:42:48 -0500

    yah

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
+from pydoc import cli
 import sys
 import discord
+import json
+import requests
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -9,17 +12,29 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
-
 bot = commands.Bot(command_prefix=os.getenv('COMMAND_PREFIX'),
                    help_command=None)
+
 @bot.command(name='balls')
-async def price(ctx):
+async def balls(ctx):
     await ctx.send(f"Joe Biden")
 
-@client.event
+@bot.command(name='dog')
+async def dog(ctx):
+    response = requests.get("https://dog.ceo/api/breeds/image/random")
+    text = response.text
+    data = json.loads(text)
+    await ctx.send(data['message'])
+
+@bot.event
 async def on_voice_state_update(member, before, after):
-  if not before.channel and after.channel and member.id == 951690798175506472:
-    channel = client.get_channel(709505921092550658)
-    await channel.send('Tyson has small balls.')
-    
+    if before.channel is None and after.channel is not None:
+        if after.channel.id == 795517016626167830: # nigs channel
+            if member.id == 951690798175506472: # tyson discord id
+                await member.guild.system_channel.send("Tyson has small balls.")
+            if member.id == 678433535920570399: # evan discord id
+                await member.guild.system_channel.send("Evan has a tiny shaft.")
+            if member.id == 535610244894556209: # jacob discord id
+                await member.guild.system_channel.send("Jacob has a MEGA DICK.")
+                
 bot.run(TOKEN)
